8ab7ec9227015063e465e445eed21e714de1b20a
canonbas03 <salihcanonbas@gmail.com>
2025-05-21 11:48:09 +0300

    03

--- obj/Debug/net8.0/SoftuniFundamentals.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoftuniFundamentals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftuniFundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24569da2826b715ec55aa4ae9fdcc6645fe4db7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75e8867f08c1e412199227391105894be948956")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftuniFundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftuniFundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

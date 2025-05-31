b24832aa2827cfefc74d0bc3c8a5b6c2e0ba44b7
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 15:54:12 +0300

    M02

---
+++ SoftuniFundamentals(BasicSyntax6)/M02.EnglishNameLastDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace M02.EnglishNameLastDigit
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+
+			int digit = number % 10;
+
+			Dictionary<int, string> nameDict = new Dictionary<int, string>()
+			{
+				{0, "zero"},
+				{1, "one"},
+				{2, "two"},
+				{3, "three"},
+				{4, "four"},
+				{5, "five"},
+				{6, "six"},
+				{7, "seven"},
+				{8, "eight"},
+				{9, "nine"},
+			};
+
+			if (nameDict.ContainsKey(digit))
+			{
+				Console.WriteLine(nameDict[digit]);
+			}
+		}
+	}
+}

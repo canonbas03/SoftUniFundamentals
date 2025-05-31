17f2d739b7395c33e95f92c4a1dd8529ea68f07e
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 14:40:52 +0300

    11

---
+++ SoftuniFundamentals(BasicSyntax6)/11.Orders/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _11.Orders
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int orderCount = int.Parse(Console.ReadLine());
+
+			double grandTotal = 0;
+			for (int i = 1; i <= orderCount; i++) 
+			{
+				double totalPrice = 0;
+				double pricePerCapsule = double.Parse(Console.ReadLine());
+				int days = int.Parse(Console.ReadLine());
+				int capsuleCount = int.Parse(Console.ReadLine());
+
+				totalPrice = (days * capsuleCount) * pricePerCapsule;
+				Console.WriteLine($"The price for the coffee is: ${totalPrice:f2}");
+				grandTotal += totalPrice;
+			}
+			Console.WriteLine($"Total: ${grandTotal:f2}");
+		}
+	}
+}

b6aa5c114e09f073601b17293c3bbeac934e38be
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 15:45:48 +0300

    More 01

---
+++ SoftuniFundamentals(BasicSyntax6)/M01.SortNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace M01.SortNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num1 = int.Parse(Console.ReadLine());
+			int num2 = int.Parse(Console.ReadLine());
+			int num3 = int.Parse(Console.ReadLine());
+
+			List<int> sortedList = new List<int>();
+
+			sortedList.Add(num1);
+			sortedList.Add(num2);
+			sortedList.Add(num3);
+
+			sortedList.Sort();
+			sortedList.Reverse();
+
+			foreach (int item in sortedList)
+			{
+
+				Console.WriteLine(item);
+			}
+		}
+	}
+}

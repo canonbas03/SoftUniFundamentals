f68d0f95aa31c2d126d76f3059a925dffee67a88
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 18:41:34 +0300

    M03.GamingStore

---
+++ SoftuniFundamentals(BasicSyntax6)/M03.Games/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace M03.Games
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, double> gameDict = new Dictionary<string, double>()
+			{
+				{"OutFall 4", 39.99},
+				{"CS: OG", 15.99},
+				{"Zplinter Zell",19.99 },
+				{"Honored 2",59.99 },
+				{"RoverWatch", 29.99},
+				{"RoverWatch Origins Edition", 39.99}
+			};
+
+			double balance = double.Parse(Console.ReadLine());
+
+			double totalSpent = 0;
+
+			string command;
+			while ((command = Console.ReadLine()) != "Game Time")
+			{
+				if (!gameDict.ContainsKey(command))
+				{
+					Console.WriteLine("Not Found");
+					continue;
+				}
+				if (gameDict.ContainsKey(command))
+				{
+					if((balance - gameDict[command]) >= 0)
+					{
+						balance -= gameDict[command];
+						totalSpent += gameDict[command];
+						if (balance > 0)
+						{
+							Console.WriteLine($"Bought {command}");
+						}
+						else if (balance == 0)
+						{
+							Console.WriteLine($"Bought {command}");
+							Console.WriteLine("Out of money!");
+							Environment.Exit(0);
+						}
+					}
+					else
+					{
+						Console.WriteLine("Too Expensive");
+						continue;
+					}
+					//continue;
+				}
+				else
+				{
+					Console.WriteLine("Not Found");
+					continue;
+				}
+			}
+				Console.WriteLine($"Total spent: ${totalSpent:f2}. Remaining: ${balance:f2}");
+
+		}
+	}
+}

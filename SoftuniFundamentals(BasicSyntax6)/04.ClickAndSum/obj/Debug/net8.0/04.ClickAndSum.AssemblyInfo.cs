20e58e7c226f759ceee0b58e22ce0e6867dc879f
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 13:30:14 +0300

    test

--- SoftuniFundamentals(BasicSyntax6)/04.ClickAndSum/obj/Debug/net8.0/04.ClickAndSum.AssemblyInfo.cs
+++ SoftuniFundamentals(BasicSyntax6)/04.ClickAndSum/obj/Debug/net8.0/04.ClickAndSum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04.ClickAndSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206e3949b2c3d3642485158585ca6246dddbc92a")]
 [assembly: System.Reflection.AssemblyProductAttribute("04.ClickAndSum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04.ClickAndSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SoftuniFundamentals(BasicSyntax6)/SoftuniFundamentals(BasicSyntax6)/obj/Debug/net8.0/SoftuniFundamentals(BasicSyntax6).AssemblyInfo.cs
+++ SoftuniFundamentals(BasicSyntax6)/SoftuniFundamentals(BasicSyntax6)/obj/Debug/net8.0/SoftuniFundamentals(BasicSyntax6).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftuniFundamentals(BasicSyntax6)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206e3949b2c3d3642485158585ca6246dddbc92a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftuniFundamentals(BasicSyntax6)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftuniFundamentals(BasicSyntax6)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

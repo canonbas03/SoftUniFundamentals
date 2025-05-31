71b63c0ac2a7aff2c677f7442f6594127b6b2098
canonbas03 <salihcanonbas@gmail.com>
2025-05-31 20:15:53 +0300

    M05.Messages

---
+++ SoftuniFundamentals(BasicSyntax6)/M05.Messages/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace M05.Messages
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int symbolCount = int.Parse(Console.ReadLine());
+			string word = string.Empty;
+			while (symbolCount >0)
+			{
+				int code = int.Parse(Console.ReadLine());
+
+				if(code==0)
+				{
+					word += " ";
+					symbolCount--;
+					continue;
+				}
+
+				int mainDigit = code % 10;
+				int digitLen = code.ToString().Length;
+
+				int offset = (mainDigit - 2) * 3;
+				if (mainDigit == 8 || mainDigit == 9)
+				{
+					offset++;
+				}
+
+				int letterIndex = (offset + digitLen -1) + 97;
+				word += (char)(letterIndex);
+				//Console.WriteLine((char)(letterIndex));
+				symbolCount--;
+			}
+			Console.WriteLine(word);
+		}
+	}
+}

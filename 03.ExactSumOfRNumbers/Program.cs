4c43bf5bf0fedabcc4b38f41aa245671b0871d49
canonbas03 <salihcanonbas@gmail.com>
2025-06-05 22:48:45 +0300

    234

---
+++ 03.ExactSumOfRNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.ExactSumOfRNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int counter = int.Parse(Console.ReadLine());
+
+			decimal total = 0;
+			for (int i = 0; i < counter; i++)
+			{
+				decimal number = decimal.Parse(Console.ReadLine());
+				total += number;
+		
+			}
+			Console.WriteLine(total);
+		}
+	}
+}
---
+++ 04.CenturiesToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.CenturiesToMinutes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			uint yearIndex = 100;
+			double dayIndex = 365.2422;
+			sbyte hourIndex = 24;
+			sbyte minuteIndex = 60;
+
+			int centuries = int.Parse(Console.ReadLine());
+
+			Console.WriteLine($"{centuries} centuries = {centuries * yearIndex} years = {(int)(centuries * yearIndex * dayIndex)} " +
+				$"days = {(int)(centuries * yearIndex * dayIndex * hourIndex)} hours = {(int)(centuries * yearIndex * dayIndex * hourIndex * minuteIndex)} minutes");
+		}
+	}
+}

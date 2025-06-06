c98c25d7a554d97be4b1df304da0615b4cd26342
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 10:52:14 +0300

    123

---
+++ 03.Elevator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _03.Elevator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int persons = int.Parse(Console.ReadLine());
+			int capacity = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(Math.Ceiling((double)persons / capacity));
+		}
+	}
+}

a66e5899754b0f49d581e24b7dc957d134861828
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 09:31:45 +0300

    9

--- 08.TownInfo/Program.cs
+++ 08.TownInfo/Program.cs
@@ -4,7 +4,11 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			string townName = Console.ReadLine();	
+			int population = int.Parse(Console.ReadLine());
+			int area = int.Parse(Console.ReadLine());
+
+			Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
 		}
 	}
 }
---
+++ 09.CharToString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _09.CharToString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string finalWord = string.Empty;
+
+			for (int i = 0; i < 3; i++)
+			{
+				char ch = Console.ReadLine()[0];
+				finalWord += ch;
+			}
+			Console.WriteLine(finalWord);
+		}
+	}
+}

5872565c21bac64bf0337ec344ae42a1113c1c7f
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 09:25:06 +0300

    78

---
+++ 07.ContactNames1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _07.ContactNames1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = Console.ReadLine();
+			string surname = Console.ReadLine();
+			string delimeter = Console.ReadLine();
+
+			Console.WriteLine($"{name}{delimeter}{surname}");
+		}
+	}
+}

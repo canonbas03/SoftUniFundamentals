910ad9c382ccaaea1b2189b2de07fd103e8ee067
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 10:00:48 +0300

    Update Program.cs

--- 12.RefactorSpecialNumbers/Program.cs
+++ 12.RefactorSpecialNumbers/Program.cs
@@ -12,27 +12,20 @@
 			bool isSpecial = false;
 
 			for (int number = 1; number <= counter; number++)
-
 			{
-
 				placeholder = number;
 
 				while (number > 0)
 
 				{
-
 					total += number % 10;
-
 					number = number / 10;
-
 				}
 
 				isSpecial = (total == 5) || (total == 7) || (total == 11);
-
 				Console.WriteLine("{0} -> {1}", placeholder, isSpecial);
 
 				total = 0;
-
 				number = placeholder;
 
 			}

3eb9f10d36754234aaff41f6cc34a60b2a4cf11c
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 23:00:34 +0300

    456

---
+++ 04.SumOfChars/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04.SumOfChars
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			int asciiSum = 0;
+
+			for (int i = 0; i < number; i++)
+			{
+				char ch = Console.ReadLine()[0];
+				asciiSum+= (int)ch;
+			}
+			Console.WriteLine($"The sum equals: {asciiSum}");
+
+		}
+	}
+}
---
+++ 06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int index = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < index; i++)
+			{
+				for (int j = 0; j < index; j++)
+				{
+					for (int k = 0; k < index; k++)
+					{
+						Console.WriteLine($"{(char)('a' + i)}{(char)('a' + j)}{(char)('a' + k)}");
+					}
+				}
+			}
+		}
+	}
+}

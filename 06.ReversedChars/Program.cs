c62fb2a893b25ea57cdfa88fb3d2ad3492388480
canonbas03 <salihcanonbas@gmail.com>
2025-06-06 09:16:47 +0300

    06

---
+++ 06.ReversedChars/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.ReversedChars
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<char> list = new List<char>();
+
+			for (int i = 0;i < 3; i++)
+			{
+				char ch = char.Parse(Console.ReadLine());
+				list.Add(ch);
+			}
+
+			list.Reverse();
+
+			foreach (char ch in list)
+			{
+				Console.Write(ch + " ");
+			}
+		}
+	}
+}

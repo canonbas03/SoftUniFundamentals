b6c4a37d1147ba80a351dd9df91e796202b12d6e
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 12:15:01 +0300

    12

---
+++ M01.DataTypeFinder/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace M01.DataTypeFinder
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string command = "";
+			while ((command = Console.ReadLine()) != "END")
+			{
+                if(int.TryParse(command, out int intVal))
+				{
+                    Console.WriteLine($"{command} is integer type");
+                } 
+				else if(float.TryParse(command, out float floatVal))
+				{
+                    Console.WriteLine($"{command} is floating point type");
+                } 
+				else if(char.TryParse(command, out char charVal))
+				{
+                    Console.WriteLine($"{command} is character type");
+                }  
+				else if(bool.TryParse(command, out bool boolVal))
+				{
+                    Console.WriteLine($"{command} is boolean type");
+                }
+				else
+				{
+					Console.WriteLine($"{command} is string type");
+				}
+			}
+		}
+	}
+}
---
+++ M02.FromLeftToRight/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace M02.FromLeftToRight
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int numbers = int.Parse(Console.ReadLine());
+			for (int i = 0; i < numbers; i++)
+			{
+
+				string firstNumber = string.Empty;
+				string secondNumber = string.Empty;
+				string numberStack = Console.ReadLine();
+				for (int j = 0; j < numberStack.Length; j++)
+				{
+					if (numberStack[j] != ' ')
+					{
+						firstNumber += numberStack[j];
+					}
+					else
+					{
+						for (int s = firstNumber.Length + 1; s < numberStack.Length; s++)
+						{
+							secondNumber += numberStack[s];
+						}
+						break;
+					}
+				}
+
+				//Console.WriteLine(firstNumber);
+				//Console.WriteLine(secondNumber);
+
+				long intFirstNum = 0;
+				long intSecondNum = 0;
+				long.TryParse(firstNumber, out intFirstNum);
+				long.TryParse(secondNumber, out intSecondNum);
+
+				int totalOfDigits = 0;
+				if (intFirstNum >= intSecondNum)
+				{
+					intFirstNum = Math.Abs(intFirstNum);
+					while (intFirstNum > 0)
+					{
+						int lastDigit = (int)(intFirstNum % 10);
+						totalOfDigits += lastDigit;
+						intFirstNum /= 10;
+					}
+				}
+				else
+				{
+					intSecondNum = Math.Abs(intSecondNum);
+					while (intSecondNum > 0)
+					{
+						int lastDigit = (int)(intSecondNum % 10);
+						totalOfDigits += lastDigit;
+						intSecondNum /= 10;
+					}
+				}
+				Console.WriteLine(totalOfDigits);
+			}
+		}
+	}
+}

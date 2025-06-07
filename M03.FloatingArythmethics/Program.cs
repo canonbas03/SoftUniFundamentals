3b44f2bcc1ba289659d522979dbc52071543ca30
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 12:21:07 +0300

    3

---
+++ M03.FloatingArythmethics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace M03.FloatingArythmethics
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			decimal eps = 0.000001m;
+
+			double first = double.Parse(Console.ReadLine());
+			double second = double.Parse(Console.ReadLine());
+
+			decimal result = (decimal)(first - second);
+
+			if(Math.Abs(result) < eps)
+			{
+                Console.WriteLine("True");
+            }
+			else
+			{
+                Console.WriteLine("False");
+            }
+		}
+	}
+}

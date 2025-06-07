7867095b3c3a0192b347484658e95ec93f8d3b90
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 14:49:07 +0300

    06m

---
+++ M06.BalancedBrackets/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace M06.BalancedBrackets
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int lineNum = int.Parse(Console.ReadLine());
+
+			int leftBrCounter = 0;
+			int rightBrCounter = 0;
+			bool isClosed = true;
+			for (int i = 0; i < lineNum; i++)
+			{
+				string input = Console.ReadLine();
+
+				if (input == "(")
+				{
+					leftBrCounter++;
+				}
+
+				else if (input == ")")
+				{
+					rightBrCounter++;
+					if (leftBrCounter == 1 && rightBrCounter == 1)
+					{
+						isClosed = true;
+						leftBrCounter = 0;
+						rightBrCounter = 0;
+					}
+					else
+					{
+						isClosed = false;
+					}
+
+				}
+			}
+			if (isClosed)
+			{
+				Console.WriteLine("BALANCED");
+			}
+			else
+			{
+				Console.WriteLine("UNBALANCED");
+			}
+		}
+	}
+}

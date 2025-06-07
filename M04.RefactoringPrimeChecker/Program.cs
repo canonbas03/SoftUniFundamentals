050953a3fac09b1fd27f065d434fb06af8aeda6e
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 12:30:42 +0300

    m4

---
+++ M04.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace M04.RefactoringPrimeChecker
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int end = int.Parse(Console.ReadLine());
+
+			for (int initial = 2; initial <= end; initial++)
+
+			{
+
+				bool isPrime = true;
+
+				for (int divider = 2; divider < initial; divider++)
+
+				{
+
+					if (initial % divider == 0)
+
+					{
+
+						isPrime = false;
+
+						break;
+
+					}
+
+				}
+
+				Console.WriteLine("{0} -> {1}", initial, isPrime.ToString().ToLower());
+
+			}
+		}
+	}
+}

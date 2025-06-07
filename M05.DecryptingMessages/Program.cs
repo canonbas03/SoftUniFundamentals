946ea93882405045e7e22d432ff33e834db8d6f0
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 12:38:00 +0300

    m5

---
+++ M05.DecryptingMessages/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace M05.DecryptingMessages
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int key = int.Parse(Console.ReadLine());
+			int lineNum = int.Parse(Console.ReadLine());
+			string finalWord = string.Empty;
+			for (int i = 0; i < lineNum; i++)
+			{
+				char ch = char.Parse(Console.ReadLine());
+				char decrypted = (char)((int)(ch) + key);
+
+				finalWord += decrypted;
+			}
+            Console.WriteLine(finalWord);
+        }
+	}
+}

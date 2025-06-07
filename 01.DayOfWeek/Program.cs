bf0615e1c29780ea8bd4c55f1cf0328a6ea2f1d7
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 15:03:49 +0300

    12

---
+++ 01.DayOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.DayOfWeek
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+			int index = int.Parse(Console.ReadLine());
+			if(index <= 7 && index >0)
+			Console.WriteLine(days[index-1]);
+
+			else { Console.WriteLine("Invalid day!"); }
+        }
+	}
+}

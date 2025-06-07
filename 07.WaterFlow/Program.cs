6a38302cfbae8af9dcbcd9fd34c9f7b98304eb4a
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 08:50:32 +0300

    7

---
+++ 07.WaterFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlTypes;
+
+namespace _07.WaterFlow
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int capacity = 255;
+
+			int lineNum = int.Parse(Console.ReadLine());
+			int totalLiters = 0;
+
+			for (int i = 0; i < lineNum; i++)
+			{
+				int liters = int.Parse(Console.ReadLine());
+				if(totalLiters + liters <= 255)
+				{
+					totalLiters += liters;
+				}
+				else
+				{
+                    Console.WriteLine("Insufficient capacity!");
+                }
+			}
+            Console.WriteLine(totalLiters);
+        }
+	}
+}

3b5e428b17d5a63fe93a7daeb8dbae08b6160858
canonbas03 <salihcanonbas@gmail.com>
2025-06-07 10:19:12 +0300

    9

---
+++ 09.SpiceMustFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _09.SpiceMustFlow
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int startYield = int.Parse(Console.ReadLine());
+
+			int day = 0;
+			int totalExtracted = 0;
+			while (startYield >= 100)
+			{
+				day++;
+				totalExtracted += startYield;
+				totalExtracted -= 26;
+				startYield -= 10;
+			}
+			if (totalExtracted >= 26)
+			{
+				totalExtracted -= 26;
+			}
+
+			Console.WriteLine(day);
+			Console.WriteLine(totalExtracted);
+		}
+	}
+}

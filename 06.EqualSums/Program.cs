b9777d1a507a2b481107fe7db63f557dbf58e6b0
canonbas03 <salihcanonbas@gmail.com>
2025-06-10 19:06:47 +0300

    6

---
+++ 06.EqualSums/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _06.EqualSums
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			bool foundEl = false;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				if (numbers.Length == 1)
+				{
+                    Console.WriteLine("0");
+					foundEl = true;
+					break;
+                }
+				int sumRight = 0, sumLeft = 0;
+				for (int k = 0; k < i; k++)
+				{
+					sumLeft += numbers[k];
+				}
+				for (int j = i + 1; j < numbers.Length; j++)
+				{
+					sumRight += numbers[j];
+				}
+
+				if (sumLeft == sumRight)
+				{
+					foundEl = true;
+					Console.WriteLine(i);
+					break;
+				}
+			}
+			if (!foundEl)
+			{
+				Console.WriteLine("no");
+			}
+
+		}
+
+
+	}
+}
+
+// 1 2 3 3

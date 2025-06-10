f22eda866f817b0663570687210d9b5d660b6a38
canonbas03 <salihcanonbas@gmail.com>
2025-06-10 19:24:09 +0300

    6

---
+++ 06.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.MaxSequenceOfEqualElements
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			int bestEl = 0, bestCount = 0;
+			for (int i = 0; i < elements.Length; i++)
+			{
+				int currentEl = elements[i];
+				int counter = 1;
+
+
+				for (int j = i + 1; j < elements.Length; j++)
+				{
+					if (currentEl == elements[j])
+					{
+						counter++;
+					}
+					else
+					{
+						break;
+					}
+				}
+				if (counter > bestCount)
+				{
+					bestCount = counter;
+					bestEl = currentEl;
+				}
+			}
+			for (int i = 0;i < bestCount; i++)
+			{
+                Console.Write(bestEl + " ");
+            }
+		}
+	}
+}

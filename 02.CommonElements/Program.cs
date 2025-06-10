6004b18b5a63af5c6e0569e50568aa721b72b742
canonbas03 <salihcanonbas@gmail.com>
2025-06-10 16:40:40 +0300

    2 5 12

---
+++ 02.CommonElements/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.CommonElements
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string[] firstArray = input.Split();
+
+			input = Console.ReadLine();
+			string[] secondArray = input.Split();
+
+			for (int i = 0; i < secondArray.Length; i++)
+			{
+				for (int j = 0; j < firstArray.Length; j++)
+				{
+					if (secondArray[i] == firstArray[j])
+					{
+						Console.Write(secondArray[i] + " ");
+						break;
+					}
+				}
+			}
+
+		}
+	}
+}
---
+++ 05.TopIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.TopIntegers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				bool isTop = true;
+				for (int j = i + 1; j < numbers.Length; j++)
+				{
+					int current = numbers[i], compare = numbers[j];
+					if (numbers[i] < numbers[j])
+					{
+						isTop = false;
+						break;
+					}
+				}
+				if (isTop)
+				{
+					Console.Write(numbers[i]  +" ");
+				}
+			}
+		}
+	}
+}
+
+// 14 24 3 19 15 17

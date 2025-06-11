36f95590742cf361fbfea132dbc8f53a7f4499d6
canonbas03 <salihcanonbas@gmail.com>
2025-06-11 10:05:50 +0300

    10

---
+++ Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = Math.Abs(int.Parse(Console.ReadLine()));
+			Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+		}
+
+		static int GetMultipleOfEvenAndOdds(int number)
+		{
+			return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+		}
+
+		static int GetSumOfEvenDigits(int number)
+		{
+			int totalSum = 0;
+			while (number > 0)
+			{
+				int lastDigit = number % 10;
+				if (lastDigit % 2 == 0)
+				{
+					totalSum += lastDigit;
+				}
+				number = number / 10;
+			}
+			return totalSum;
+		}
+
+		static int GetSumOfOddDigits(int number)
+		{
+			int totalSum = 0;
+			while (number > 0)
+			{
+				int lastDigit = number % 10;
+				if (lastDigit % 2 != 0)
+				{
+					totalSum += lastDigit;
+				}
+				number = number / 10;
+			}
+			return totalSum;
+		}
+	}
+}

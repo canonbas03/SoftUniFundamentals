deebccb4a0ce163b65740e80db6a148f57bd4852
canonbas03 <salihcanonbas@gmail.com>
2025-06-11 09:18:36 +0300

    34

---
+++ Methods/03.Calculations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.Calculations
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string operation = Console.ReadLine();
+			int number1 = int.Parse(Console.ReadLine());
+			int number2 = int.Parse(Console.ReadLine());
+
+			switch (operation)
+			{
+				case "add":
+					CalculateAdd(number1, number2);
+					break;
+				case "subtract":
+					CalculateSub(number1, number2);
+					break;
+				case "multiply":
+					CalculateMul(number1, number2);
+					break;
+				case "divide":
+					CalculateDiv(number1, number2);
+					break;
+				default:
+					break;
+			}
+		}
+
+		static void CalculateAdd(int num1, int num2)
+		{
+            Console.WriteLine(num1 + num2);
+        }
+		static void CalculateSub(int num1, int num2)
+		{
+            Console.WriteLine(num1 - num2);
+        }
+		static void CalculateMul(int num1, int num2)
+		{
+            Console.WriteLine(num1 * num2);
+        }
+		static void CalculateDiv(int num1, int num2)
+		{
+            Console.WriteLine(num1 / num2);
+        }
+	}
+}
---
+++ Methods/04.printTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.printTriangle
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PrintTriangle(1, int.Parse(Console.ReadLine()));
+		}
+
+
+		static void PrintTriangle(int start, int end)
+		{
+			for (int i = start; i < end; i++)
+			{
+				PrintLine(1, i);
+			}
+
+			for (int i = end; i >= start; i--)
+			{
+				PrintLine(start, i);
+			}
+		}
+		static void PrintLine(int start, int end)
+		{
+			for (int i = start; i <= end; i++)
+			{
+				Console.Write(i + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}

6ae88e062bccfaeb3c1d7f8e73b63b7fc7b22cb7
canonbas03 <salihcanonbas@gmail.com>
2025-06-11 08:42:55 +0300

    1

--- Methods/01.SignOfIntegers/Program.cs
+++ Methods/01.SignOfIntegers/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace _01.SignOfIntegers
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _01.SignOfIntegers
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			SignCheck(int.Parse(Console.ReadLine()));
+		}
+		static void SignCheck(int i)
+		{
+			if(i < 0)
+			{
+                Console.WriteLine($"The number {i} is negative. ");
+			}
+			else if(i > 0)
+			{
+                Console.WriteLine($"The number {i} is positive. ");
+            }
+			else
+			{
+                Console.WriteLine($"The number {i} is zero. ");
+            }
 		}
 	}
 }

2cf5a447e880a9c40095945375e36478e0d5ecc2
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 20:03:48 +0300

    2

---
+++ Exercise_Methods/02.VowelsCount/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(VowelsCount(word));
+        }
+
+        private static string VowelsCount(string? word)
+        {
+            int counter = default;
+            foreach (char symbol in word)
+            {
+                if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u' ||
+                    symbol == 'A' || symbol == 'E' || symbol == 'I' || symbol == 'O' || symbol == 'U')
+                {
+                    counter++;
+                }
+            }
+            return counter.ToString(); 
+        }
+    }
+}

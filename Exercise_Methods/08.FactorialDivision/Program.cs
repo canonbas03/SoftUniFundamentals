9ee011fbe0c70f8f40e6f3802aa6dd2fdb3a1094
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 21:13:42 +0300

    8

---
+++ Exercise_Methods/08.FactorialDivision/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            double result = Factorial(number1) / Factorial(number2);
+            Console.WriteLine($"{result:f2}");
+        }
+
+        private static int Factorial(int number)
+        {
+            int result = 1;
+            for (int i = number; i > 0; i--) 
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}

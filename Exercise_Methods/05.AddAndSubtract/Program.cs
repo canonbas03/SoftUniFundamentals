57046259fe2b0af16b9a23424736315ff54d57e4
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 20:45:17 +0300

    5

---
+++ Exercise_Methods/05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SubtractTwo(SumTwo(number1,number2),number3));
+        }
+
+        private static int SubtractTwo(int a, int b)
+        {
+           return a - b;    
+        }
+
+        private static int SumTwo(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

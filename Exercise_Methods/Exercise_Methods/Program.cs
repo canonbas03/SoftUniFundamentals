3f9f44ca4f96ce05e8db29d3dfc6fe84576fd388
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 19:56:54 +0300

    1

--- Exercise_Methods/Exercise_Methods/Program.cs
+++ Exercise_Methods/Exercise_Methods/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Exercise_Methods
+﻿
+namespace _01.Same3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BiggestOfTree(number1, number2, number3));
+        }
+
+        private static int BiggestOfTree(int number1, int number2, int number3)
+        {
+            if (number1 <= number2 && number1 <= number3)
+            {
+                return number1;
+            }
+            else if (number2 <= number1 && number2 <= number3)
+            {
+                return number2;
+            }
+            return number3;
         }
     }
 }

cec30c1084b8bb2274f27424a272bf6ea799fd85
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 20:14:07 +0300

    3

---
+++ Exercise_Methods/03.CharsInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _03.CharsInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           char first = char.Parse(Console.ReadLine());
+           char second = char.Parse(Console.ReadLine());
+            Console.WriteLine(CharsBetween(first, second));
+        }
+
+        private static string CharsBetween(char first, char second)
+        {
+            char firstChar = first, secondChar = second;
+            if(second < first)
+            {
+                firstChar = second;
+                secondChar = first;
+            }
+
+            string result = string.Empty;
+            for(int i = firstChar+1; i < secondChar; i++) 
+            {
+                result += (char)i + " ";
+            }
+            return result;
+        }
+    }
+}

6646c16e2bd55b11644ab3a83e6df5856884db42
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 20:57:27 +0300

    7

---
+++ Exercise_Methods/07.NxNMatrics/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _07.NxNMatrics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            MakeMatrics(number);
+        }
+
+        private static void MakeMatrics(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int k = 0; k < number; k++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

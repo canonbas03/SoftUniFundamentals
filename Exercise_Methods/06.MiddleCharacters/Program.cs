81454f42f9a2e0a67a82f4d27747b5ec653bfc5e
canonbas03 <salihcanonbas@gmail.com>
2025-06-16 20:54:48 +0300

    6

---
+++ Exercise_Methods/06.MiddleCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            ShowMiddle(text);
+        }
+
+        private static void ShowMiddle(string? text)
+        {
+            int middle = (text.Length / 2) - 1;
+            if(text.Length % 2 == 0)
+            {
+                Console.WriteLine($"{text[middle]}{text[middle + 1]}");
+            }
+            else
+            {
+                Console.WriteLine(text[middle+1]);
+            }
+        }
+    }
+}

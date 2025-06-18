0ed657f970be775798846c8173e7e38ca89bdb7e
canonbas03 <salihcanonbas@gmail.com>
2025-06-18 12:40:29 +0300

    2

---
+++ 01.SumAdjEqNumbers/02.GaussTrick/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int halfIndex = numbers.Count / 2;
+
+            for (int i = 0; i < halfIndex; i++)
+            {
+                numbers[i] += numbers[numbers.Count-1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}

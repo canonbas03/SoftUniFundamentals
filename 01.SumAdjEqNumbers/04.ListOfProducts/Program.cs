23270c4a516fb29f3d8098c5b01ce9f5a31d159d
canonbas03 <salihcanonbas@gmail.com>
2025-06-18 13:27:37 +0300

    4

---
+++ 01.SumAdjEqNumbers/04.ListOfProducts/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int listSize = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < listSize; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+            for (int i = 0;i < products.Count;i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}

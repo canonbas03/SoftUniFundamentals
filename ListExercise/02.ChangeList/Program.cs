948504e20888683847b1d0ae9b8f07cb9cef94f9
canonbas03 <salihcanonbas@gmail.com>
2025-06-20 18:38:44 +0300

    2

---
+++ ListExercise/02.ChangeList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] commands = command.Split();
+
+                if (commands[0] == "Delete")
+                {
+                    int elementToDel = int.Parse(commands[1]);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == elementToDel)
+                        {
+                            list.Remove(elementToDel);
+                            i--;
+                        }
+                    }
+                }
+                else
+                {
+                    int elementToInsert = int.Parse(commands[1]);
+                    int elementPos = int.Parse(commands[2]);
+                    list.Insert(elementPos, elementToInsert);
+                }
+            }
+        }
+    }
+}

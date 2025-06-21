2e2081779cdb3fe8c530abda701c1093dc41ace9
canonbas03 <salihcanonbas@gmail.com>
2025-06-21 11:06:13 +0300

    5

---
+++ ListExercise/05.BombNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> bomb = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int number = bomb[0];
+            int power = bomb[1];
+
+            while(list.Contains(number))
+            {
+                int bombIndex = list.IndexOf(number);
+
+                int leftIndex = Math.Max(0, bombIndex - power);
+                int rightIndex = Math.Min(list.Count-1, bombIndex + power);
+
+                int rangeToRemove = rightIndex - leftIndex + 1;
+                list.RemoveRange(leftIndex, rangeToRemove);
+            }
+            Console.WriteLine(list.Sum());
+        }
+    }
+}

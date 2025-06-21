84361bb84807ff184788c13d28395ce3ca42bb98
canonbas03 <salihcanonbas@gmail.com>
2025-06-21 11:43:22 +0300

    7

---
+++ ListExercise/07.AppendArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                 .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            List<string> finalList = new List<string>();
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                List<string> partList = list[i].Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                finalList.AddRange(partList);
+            }
+        }
+    }
+}

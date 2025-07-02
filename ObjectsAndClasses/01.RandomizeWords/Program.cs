2d179ec40e69a24884b7a7899bfea246209c9a26
canonbas03 <salihcanonbas@gmail.com>
2025-07-02 19:27:53 +0300

    01

---
+++ ObjectsAndClasses/01.RandomizeWords/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split();
+
+        Random random = new Random();
+        for (int i = 0; i < words.Length; i++)
+        {
+            SwapElements(i,random.Next(0,words.Length), words);
+        }
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    static void SwapElements(int firstIndex, int secondIndex, string[] elements)
+    {
+        string temp = elements[firstIndex];
+        elements[firstIndex] = elements[secondIndex];
+        elements[secondIndex] = temp;
+    }
+}

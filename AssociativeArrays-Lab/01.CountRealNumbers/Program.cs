6908febcb826f035e8262bafaa2592035d289e66
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 12:21:23 +0300

    02

---
+++ AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> numbersCount = new();
+
+            foreach (int number in numbers)
+            {
+                if (!numbersCount.ContainsKey(number))
+                {
+                    numbersCount[number] = 0;
+                }
+                numbersCount[number]++;
+            }
+
+            foreach((int key, int occurence) in numbersCount)
+            {
+                Console.WriteLine($"{key} -> {occurence}");
+            }
+        }
+    }
+}
---
+++ AssociativeArrays-Lab/02.OddOccurences/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.OddOccurences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().ToLower().Split();
+            Dictionary<string, int> inputCounts = new Dictionary<string, int>();
+
+            foreach (string input in inputs)
+            {
+                if (!inputCounts.ContainsKey(input))
+                {
+                    inputCounts[input] = 0;
+                }
+                inputCounts[input]++;
+            }
+
+            foreach((string word, int count) in inputCounts)
+            {
+                if(count % 2 != 0)
+                {
+                    Console.Write(word + " ");
+                }
+            }
+        }
+    }
+}

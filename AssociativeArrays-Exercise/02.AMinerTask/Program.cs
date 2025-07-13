ea9d2a344b88a41ca82727a432b5ac2748a322f9
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 17:44:35 +0300

    0203

---
+++ AssociativeArrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, uint> resourceQuantities = new();
+            string input;
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string resource = input;
+                uint quantity = uint.Parse(Console.ReadLine());
+
+                if (!resourceQuantities.ContainsKey(resource))
+                {
+                    resourceQuantities[resource] = default;
+                }
+
+                resourceQuantities[resource] += quantity;
+            }
+            foreach (KeyValuePair<string, uint> resources in resourceQuantities)
+            {
+                Console.WriteLine($"{resources.Key} -> {resources.Value}");
+            }
+
+        }
+    }
+}
---
+++ AssociativeArrays-Exercise/03.Orders/Program.cs
@@ -0,0 +1,59 @@
+﻿
+namespace _03.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Product> productsMap = new();
+            string input;
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] arguments = input.Split();
+
+                string name = arguments[0];
+                decimal price = decimal.Parse(arguments[1]);
+                decimal quantity = decimal.Parse(arguments[2]);
+
+                if (!productsMap.ContainsKey(name))
+                {
+                    Product newProduct = new Product(name, price, quantity);
+                    productsMap.Add(name, newProduct);
+                }
+                else
+                {
+                    productsMap[name].Update(price, quantity);
+                }
+            }
+            foreach (KeyValuePair<string, Product> pair in productsMap)
+            {
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+
+    public class Product
+    {
+        public Product(string name, decimal price, decimal quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity += quantity;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+
+        internal void Update(decimal price, decimal quantity)
+        {
+            Price = price;
+            Quantity += quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {Price * Quantity:f2}";
+        }
+    }
+}

f0e425b9cc14e7d458dc97e94b710bb5bb40c0f4
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 13:51:25 +0300

    04
    Anonym methods, Linq

---
+++ AssociativeArrays-Lab/04.WordFilter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsEven = Console.ReadLine().Split().Where(w => w.Length % 2 == 0).ToArray();
+            Console.WriteLine(string.Join("\n",wordsEven));
+        }
+    }
+}

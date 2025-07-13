51f0e047cee1a9c3e4911efb75afb2c8cc69b412
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 18:14:13 +0300

    05

---
+++ AssociativeArrays-Exercise/05Courses/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courseStudents = new();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split(" : ");
+                string course = arguments[0];
+                string student = arguments[1];
+
+                if (!courseStudents.ContainsKey(course))
+                {
+                    courseStudents[course] = new List<string>();
+                }
+                courseStudents[course].Add(student);
+            }
+            foreach (KeyValuePair<string, List<string>> pair in courseStudents)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value.Count}");
+                Console.WriteLine(string.Join("\n",pair.Value));
+            }
+        }
+    }
+}

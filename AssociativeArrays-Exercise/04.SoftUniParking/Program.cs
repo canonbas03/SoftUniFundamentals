94b84fd6fa7c14b0f421a30d28b8d953fa056bc4
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 18:01:03 +0300

    04

---
+++ AssociativeArrays-Exercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace _04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> usersPlates = new();
+            int commandsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string command = input[0];
+                string userName = input[1];
+
+                if (command == "register")
+                {
+                    string licensePlate = input[2];
+                    if (!usersPlates.ContainsKey(userName))
+                    {
+                        usersPlates[userName] = licensePlate;
+                        Console.WriteLine($"{userName} registered {licensePlate} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                    }
+                }
+                else // unregister
+                {
+                    if (!usersPlates.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: user {userName} not found");
+                    }
+                    else
+                    {
+                        usersPlates.Remove(userName);
+                        Console.WriteLine($"{userName} unregistered successfully");
+                    }
+                }
+            }
+            foreach (KeyValuePair<string, string> pairs in usersPlates)
+            {
+                Console.WriteLine($"{pairs.Key} => {pairs.Value}");
+            }
+        }
+    }
+}

f1695a673bb97b2c0444a6483e49ab267663f111
canonbas03 <salihcanonbas@gmail.com>
2025-07-13 15:41:46 +0300

    01

---
+++ AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> charsCount = new();
+            char[] chars = Console.ReadLine().ToCharArray();
+
+            foreach (char c in chars)
+            {
+                if (c != ' ')
+                {
+                    if (!charsCount.ContainsKey(c))
+                    {
+                        charsCount[c] = 0;
+                    }
+                    charsCount[c]++;
+                }
+            }
+            foreach ((char letter, int count) in charsCount)
+            {
+                Console.WriteLine($"{letter} -> {count}");
+            }
+        }
+    }
+}

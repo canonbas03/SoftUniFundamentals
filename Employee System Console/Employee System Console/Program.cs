c8e18cad0ac9b5e8d0948a198babb019d3cce203
canonbas03 <salihcanonbas@gmail.com>
2025-07-19 14:21:08 +0300

    employees

---
+++ Employee System Console/Employee System Console/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Employee_System_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.Name = "hello";
+            Console.WriteLine(test);
+        }
+
+        public void AddEmployee(string id)
+        {
+            if()
+        }
+    }
+    class Employee
+    {
+        public int Id { get; set; } // PK
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int Age { get; set; }
+    }
+
+    
+}
---
+++ Employee System Console/Employee System Console/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee_System_Console
+{
+    class Test
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name + Name;
+        }
+    }
+}

52fd5f28ba38889f289006870284d3ccc6032f64
canonbas03 <salihcanonbas@gmail.com>
2025-07-23 14:34:07 +0300

    0205

---
+++ 02.CharacterMultiplier/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string firstWord = words[0];
+            string secondWord = words[1];
+
+            Console.WriteLine(Sum(firstWord, secondWord));
+        }
+
+        public static int Sum(string firstWord, string secondWord)
+        {
+            int result = default;
+            int maxLength = Math.Max(firstWord.Length, secondWord.Length);
+            for (int index = 0; index < maxLength; index++)
+            {
+                if(index < firstWord.Length && index < secondWord.Length)
+                {
+                    result += firstWord[index] * secondWord[index];
+                }
+                else if(index < firstWord.Length)
+                {
+                    result += firstWord[index];
+                }
+                else if(index < secondWord.Length)
+                {
+                    result += secondWord[index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+
+            char[] resultChars = new char[bigNumber.Length + 1];
+            string result = string.Empty;
+            if (multiplier == 0 || bigNumber == "0")
+            {
+                result = "0";
+            }
+            else
+            {
+                int carry = 0;
+                for (int i = bigNumber.Length - 1; i >= 0; i--)
+                {
+                    int digit = int.Parse(bigNumber[i].ToString());
+                    int product = digit * multiplier + carry;
+
+                    resultChars[i + 1] = (char)((product % 10) + '0');
+                    carry = product / 10;
+                }
+
+                if (carry != 0)
+                {
+                    resultChars[0] = (char)(carry + '0');
+                }
+                result = new string(resultChars).Trim();
+            }
+                Console.WriteLine(result);
+        }
+    }
+}

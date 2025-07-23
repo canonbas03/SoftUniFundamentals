86e4354ffacfebca667db13f7555394f9cea13cb
canonbas03 <salihcanonbas@gmail.com>
2025-07-24 00:09:30 +0300

    0607

---
+++ 06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = Console.ReadLine().ToCharArray();
+            string noRepeat = string.Empty;
+            char lastChar = default;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char character = word[i];
+                if (character != lastChar)
+                {
+                    noRepeat += character;
+                }
+                lastChar = character;
+            }
+            Console.WriteLine(noRepeat);
+        }
+    }
+}
---
+++ 07.StringExplosion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            int explosionStrength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    result += '>';
+                    int strength = input[i + 1] - '0';
+                    explosionStrength += strength;
+                }
+                else if(explosionStrength > 0)
+                {
+                    explosionStrength--;
+                }
+                else
+                {
+                    result += input[i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

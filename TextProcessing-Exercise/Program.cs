0bb0d1c4d7c63afa58a243c9890e79c29e854511
canonbas03 <salihcanonbas@gmail.com>
2025-07-23 12:54:31 +0300

    01

---
+++ TextProcessing-Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextProcessing_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

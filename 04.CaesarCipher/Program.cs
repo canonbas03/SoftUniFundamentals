5da37dc99e1fd6b77f41ceb4a794690de8abb9a6
canonbas03 <salihcanonbas@gmail.com>
2025-07-23 22:02:49 +0300

    04

---
+++ 04.CaesarCipher/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+           
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] += (char)3;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

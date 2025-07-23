0fe2067bdcc106e7e0b87e8e5edbaa7e405b5425
canonbas03 <salihcanonbas@gmail.com>
2025-07-23 13:11:32 +0300

    01

--- 01.ValidUsernames/Program.cs
+++ 01.ValidUsernames/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (string username in usernames)
+            {
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    bool isValid = username.All(character => char.IsLetterOrDigit(character) || character == '-' || character == '_');
+                    //bool isLegal = true;
+                    //foreach (char ch in username)
+                    //{
+                    //    if (char.IsLetterOrDigit(ch) || ch == '-' || ch == '_')
+                    //    {
+
+                    //    }
+                    //    else
+                    //    {
+                    //        isLegal = false;
+                    //        continue;
+                    //    }
+                    //}
+                    if (isValid)
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+            }
         }
     }
 }

6f82fe0eb5a9d55b4bc8938986415cfdc65d1dcc
canonbas03 <salihcanonbas@gmail.com>
2025-07-26 17:13:19 +0300

    02

---
+++ RegularExpressions-LAB/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    /*+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[ ]?\+359([ -])[\d]{1}\1[\d]{3}\1[\d]{4}\b";
+            string phones = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollecton = regex.Matches(phones);
+
+            Console.WriteLine(string.Join(", ",matchCollecton));
+        }
+    }
+}
